5bc29749b6ab027a3bd1b9d0ab51c6943b1cbffb
Khanh1916 <289sgsg@gmail.com>
2026-06-16 18:23:30 +0700

    update

--- database/init_passwords.php
+++ database/init_passwords.php
@@ -9,7 +9,7 @@
  * Usage: php init_passwords.php
  */
 
-$tns = '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1539))(CONNECT_DATA=(SERVICE_NAME=XE)))';
+$tns = '(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1539))(CONNECT_DATA=(SERVICE_NAME=XEPDB1)))';
 $dbUser = 'dbs401_user';
 $dbPass = 'dbs401_pass';
 
